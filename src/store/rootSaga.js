0dd81d87e09264311cc44f3d7d1890cbcc43497a
Gurpreet Kaur <gurpreet.dhillon03@gmail.com>
2024-01-23 14:04:37 -0800

    uploading files for adding simple React Native project with Redux Saga Template

---
+++ src/store/rootSaga.js
@@ -0,0 +1,9 @@
+import {all, takeLatest} from '@redux-saga/core/effects';
+import {handleGetRandomUserApi} from '../container/user/Saga';
+import {getUserList} from '../container/user/Slice';
+
+function* watchAll() {
+  yield all([takeLatest(getUserList.type, handleGetRandomUserApi)]);
+}
+
+export default watchAll;
